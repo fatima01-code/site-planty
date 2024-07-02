637c6321f6ccd374f5b472ad3dc1661738ca3c59
Omario <o.ammar@hotmail.com>
2024-07-02 19:07:56 +0200

    dernier commit

--- themes/astra-child/functions.php
+++ themes/astra-child/functions.php
@@ -22,5 +22,17 @@ function child_enqueue_styles() {
 
 }
 
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+
+function add_extra_item_to_nav_menu( $items, $args ) {
+
+    if (is_user_logged_in() && ($args->theme_location == 'primary' || $args->theme_location == 'mobile_menu')) {
+        $items .= '<li class="lienAdmin menu-item"><a href="'. admin_url() .'">Admin</a></li>';
+    }
+    return $items;
+}
+
+
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 add_filter('wpcf7_autop_or_not', '__return_false');
+
